22d95f7bd30c3daf494cd1bb2b708f5d553b1bd1
taochen <14705647738@163.com>
2023-02-14 10:59:09 +0800

    万年历

--- src/main/java/com/example/daysdemo/controller/DaysDemoController.java
+++ src/main/java/com/example/daysdemo/controller/DaysDemoController.java
@@ -5,7 +5,10 @@ import cn.hutool.core.date.DateUtil;
 import com.example.daysdemo.service.DaysDemoService;
 import com.example.daysdemo.utils.CustomTaskScheduler;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.scheduling.support.CronTrigger;
@@ -26,28 +29,21 @@ public class DaysDemoController {
     @Autowired
     private DaysDemoService daysDemoService;
 
-    @Autowired
-    private CustomTaskScheduler customTaskScheduler;
 
     //如果数据库内无值则从此值开始计算
     private String str = "2023-12-32";
 
+
     private int count;
 
     //每天十点执行,执行五十次后结束
-    @Scheduled(cron = "* * 10 * * ?")
-    private void process() {
+    @Scheduled(cron = "* * 10,11 * * ?")
+    private void process() throws Exception {
         log.info("任务执行次数：{}", count + 1);
         count++;
         // 执行50次后自动停止
         if (count >= 50) {
-            log.info("任务已执行指定次数，现在自动停止");
-            boolean cancelled = customTaskScheduler.getScheduledTasks().get(this).cancel(true);
-            if (cancelled) {
-                count = 0;
-                ScheduledMethodRunnable runnable = new ScheduledMethodRunnable(this, ReflectionUtils.findMethod(this.getClass(), "printTask"));
-                customTaskScheduler.schedule(runnable, new CronTrigger("* * 10 * * ?"));
-            }
+            log.info("任务完成");
         } else {
             Date days = daysDemoService.selectMinDays();
             if (Objects.isNull(days)) {
